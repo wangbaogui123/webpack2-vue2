688ff7f79d70fd6a126fbb4a13d1c72a362f92ca
LinkedME021 <linkedme021@LinkedME021sMBP.lan>
2017-05-26 10:51:13 +0800

    add code

--- app/app.js
+++ app/app.js
@@ -3,6 +3,10 @@ const Vue = require('vue')
 
 require('./css/style.css')
 
+
+require('./component.js')(Vue)
+
+
 require('./index.js')(Vue)
 
 
--- app/index.js
+++ app/index.js
@@ -1,9 +1,18 @@
 module.exports = function(Vue){
 	
-	var v = new Vue({
-		el: '#app',
+	require('./css/index.css')
+
+	let banner = {
+		template:"<div class='banner-box'><img src='./app/img/img1.png' /></div>"
+	} 
+
+	let v = new Vue({
+		el: '#box',
 		data: {
-		  message: 'Hello Vue!'
+		  
+		},
+		components:{
+		   'my-banner':banner	
 		}
 	})
 
